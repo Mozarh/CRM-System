b897754b09a79fe845689ce38863e31b27532add
Mozarh <lil.mozarh@yandex.ru>
2025-08-25 20:33:29 +0300

    фикс кода после чеклиста

--- src/api/todoApi.ts
+++ src/api/todoApi.ts
@@ -9,11 +9,17 @@ import axios from  'axios';
 
 const API_URL = 'https://easydev.club/api/v1';
 
+const instance = axios.create({
+  baseURL: API_URL,
+  timeout: 10000,
+  headers: { "Content-Type": "application/json" },
+})
+
 export async function getTodos(
   status?: FilterStatus
 ): Promise<MetaResponse<Todo, TodoInfo>> {
-  const response = await axios.get<MetaResponse<Todo, TodoInfo>>(
-    `${API_URL}/todos`,
+  const response = await instance.get<MetaResponse<Todo, TodoInfo>>(
+    "/todos",
     {
       params: status && status !== "all" ? {filter: status}: undefined,
     }
@@ -22,17 +28,17 @@ export async function getTodos(
 }
 
 export async function addTodo(data: TodoRequest): Promise<Todo> {
-  const response = await axios.post<Todo>(`${API_URL}/todos`, data)
+  const response = await instance.post<Todo>("/todos", data)
   return response.data
 }
 
 
 export async function updateTodo(id: number, data: TodoRequest): Promise<Todo> {
-  const response = await axios.put<Todo>(`${API_URL}/todos/${id}`, data)
+  const response = await instance.put<Todo>(`/todos/${id}`, data)
   return response.data
 }
 
 export async function deleteTodo(id: number): Promise<{ success: boolean }> {
-  await axios.delete(`${API_URL}/todos/${id}`);
+  await instance.delete(`/todos/${id}`);
   return { success: true };
 }
